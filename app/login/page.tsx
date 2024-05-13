08cd5b9836f9d6515a9f9db840fb549f3b233daf
Broy <akkewach.yodsomboon@gmail.com>
2024-05-13 14:24:47 +0700

    first

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,14 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { redirect } from 'next/navigation'
-
+interface FormData {
+  email: string;
+  password: string;
+}
 
 export default function Home() {
-  useEffect(() => {
-    redirect('/login')
-  }, []);
-
   return (
    <div>
-    <h1>Main Page</h1>
+    <h1>Login Page</h1>
    </div>
   );
 }
