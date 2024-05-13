beeac9de6b7cf9bfe206f62374691f977a2a41ea
Broy <akkewach.yodsomboon@gmail.com>
2024-05-13 14:46:55 +0700

    completed login page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,15 @@
-import Image from "next/image";
+"use client"
+import React from "react";
+
+async function getData() {
+  const api_url = "https://dummyjson.com/products";
+  const res = await fetch(api_url);
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+  return res.json();
+}
+
 
 export default function Home() {
   return (
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,70 @@
-"use client"
-import React, { useState, useEffect } from 'react';
+"use client";
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
+
 interface FormData {
   email: string;
   password: string;
 }
 
-export default function Home() {
+export default function page(): JSX.Element {
+  const [statusLogin, setStatusLogin] = useState<String>("default");
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (formData.email === "aa@bb.cc" && formData.password === "1234") {
+      console.log("Login Success!!!");
+      setStatusLogin("success");
+
+      //useRouter().push("/home");
+    } else {
+      setStatusLogin("fail");
+    }
+  };
+
+  useEffect(() => {
+    if (statusLogin === "success") {
+      redirect("/home");
+    }
+  }, [statusLogin]);
+
   return (
-   <div>
-    <h1>Login Page</h1>
-   </div>
+    <div className="pt-20 flex flex-col items-center gap-10">
+      <h1 className="text-center font-bold text-2xl"> Login</h1>
+      <input
+        id="email"
+        name="email"
+        placeholder="Email"
+        className="border-2 w-[70%] lg:w-[30%] h-10"
+        value={formData.email}
+        onChange={handleInput}
+      ></input>
+      <input
+        id="password"
+        name="password"
+        placeholder="Password"
+        className="border-2 w-[70%] lg:w-[30%] h-10"
+        value={formData.password}
+        onChange={handleInput}
+      ></input>
+      <button className=" bg-green-300  p-5" onClick={handleSubmit}>
+        เข้าสู่ระบบ
+      </button>
+      {statusLogin === "fail" && (
+        <h1 className=" text-red-500">Email or password incorrect</h1>
+      )}
+    </div>
   );
 }
